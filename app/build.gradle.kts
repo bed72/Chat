4a78155a9b1f776d8d5b8056aae214b43861d317
bed72 <gjramos1000@gmail.com>
2024-07-14 21:16:50 -0300

    :arrow_up: Configuring project dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,47 +1,71 @@
 plugins {
-    alias(libs.plugins.android.application)
-    alias(libs.plugins.jetbrains.kotlin.android)
+    alias(libs.plugins.detekt)
+    alias(libs.plugins.kotlin)
+    alias(libs.plugins.compiler)
+    alias(libs.plugins.application)
+    alias(libs.plugins.serialization)
 }
 
 android {
-    namespace = "com.bed.chat"
     compileSdk = 34
+    namespace = "com.bed.chat"
 
     defaultConfig {
-        applicationId = "com.bed.chat"
         minSdk = 29
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
+        applicationId = "com.bed.chat"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+
         vectorDrawables {
             useSupportLibrary = true
         }
     }
 
     buildTypes {
+        debug {
+            isDebuggable = true
+            applicationIdSuffix = ".debug"
+        }
+
+        register("profile") {
+            isMinifyEnabled = true
+            isShrinkResources = true
+            applicationIdSuffix = ".profile"
+            initWith(getByName("debug"))
+            signingConfig = signingConfigs.getByName("debug")
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules-staging.pro"
+            )
+        }
+
         release {
-            isMinifyEnabled = false
+            isMinifyEnabled = true
+            isShrinkResources = true
+            signingConfig = signingConfigs.getByName("debug")
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
             )
         }
     }
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
-    }
-    kotlinOptions {
-        jvmTarget = "1.8"
-    }
+
     buildFeatures {
         compose = true
     }
-    composeOptions {
-        kotlinCompilerExtensionVersion = "1.5.1"
+
+    kotlinOptions {
+        jvmTarget = "19"
     }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_19
+        targetCompatibility = JavaVersion.VERSION_19
+    }
+
     packaging {
         resources {
             excludes += "/META-INF/{AL2.0,LGPL2.1}"
@@ -50,20 +74,70 @@ android {
 }
 
 dependencies {
+    // Others
+    implementation(libs.bundles.core)
+    implementation(platform(libs.compose))
 
-    implementation(libs.androidx.core.ktx)
-    implementation(libs.androidx.lifecycle.runtime.ktx)
-    implementation(libs.androidx.activity.compose)
-    implementation(platform(libs.androidx.compose.bom))
-    implementation(libs.androidx.ui)
-    implementation(libs.androidx.ui.graphics)
-    implementation(libs.androidx.ui.tooling.preview)
-    implementation(libs.androidx.material3)
-    testImplementation(libs.junit)
-    androidTestImplementation(libs.androidx.junit)
-    androidTestImplementation(libs.androidx.espresso.core)
-    androidTestImplementation(platform(libs.androidx.compose.bom))
-    androidTestImplementation(libs.androidx.ui.test.junit4)
-    debugImplementation(libs.androidx.ui.tooling)
-    debugImplementation(libs.androidx.ui.test.manifest)
+    // Test
+    testImplementation(libs.bundles.test)
+
+    // Test Android
+    androidTestImplementation(platform(libs.compose))
+    androidTestImplementation(libs.bundles.android.test)
+
+    // Debug
+    debugImplementation(libs.bundles.debug)
+
+//    implementation(libs.ui)
+//    implementation(libs.ui.tooling)
+//    implementation(libs.ui.graphics)
+//    implementation(libs.ui.material)
+//
+//    implementation(libs.core)
+//    implementation(libs.activity)
+//    implementation(libs.lifecycle)
+//    implementation(platform(libs.compose))
+//
+//    testImplementation(libs.junit)
+//
+//    debugImplementation(libs.ui.tooling)
+//    debugImplementation(libs.ui.manifest)
+//
+//    androidTestImplementation(libs.junit)
+//    androidTestImplementation(libs.espresso)
+//    androidTestImplementation(libs.ui.junit4)
+//    androidTestImplementation(platform(libs.compose))
+}
+
+detekt {
+    toolVersion = libs.versions.detekt.get()
+
+    parallel = true
+
+    debug = false
+    allRules = false
+    ignoreFailures = false
+    buildUponDefaultConfig = false
+    disableDefaultRuleSets = false
+
+    basePath = projectDir.absolutePath
+    ignoredBuildTypes = listOf("release")
+    config.setFrom(file("$rootDir/config/detekt/detekt.yml"))
+    source.setFrom(
+        "$rootDir/app/src/main/java",
+        "$rootDir/app/src/test/java",
+        "$rootDir/app/src/androidTest/java"
+    )
+}
+
+afterEvaluate {
+    tasks.named("preBuild") {
+        dependsOn("detekt")
+    }
+}
+
+tasks.detekt.configure {
+    reports {
+        sarif.required.set(true)
+    }
 }
