39f6f1c645291dde578eda379317ea5cbc90f142
bed72 <gjramos1000@gmail.com>
2024-08-24 13:22:07 -0300

    :lipstick: Create PrimaryButton

--- app/src/main/java/com/bed/chat/presentation/components/PrimaryButton.kt
+++ app/src/main/java/com/bed/chat/presentation/components/PrimaryButton.kt
@@ -1,12 +1,15 @@
 package com.bed.chat.presentation.components
 
 import androidx.compose.runtime.Composable
+import androidx.compose.animation.animateContentSize
 
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.StrokeCap
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.aspectRatio
@@ -15,28 +18,44 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material3.Text
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.CircularProgressIndicator
 
 @Composable
 fun PrimaryButton(
     text: String,
     onClick: () -> Unit,
-    isLoading: Boolean = false,
-    modifier: Modifier = Modifier
+    modifier: Modifier = Modifier,
+    isLoading: Boolean = false
 ) {
     Button(
         modifier = modifier
-            .height(54.dp)
-            .fillMaxWidth(),
+            .height(54.dp),
+        onClick = onClick,
         enabled = !isLoading,
         shape = MaterialTheme.shapes.medium,
-        onClick = onClick
-    ) {
-        if (isLoading) CircularProgressIndicator(
-            modifier = Modifier.size(24.dp).aspectRatio(1f),
-            strokeCap = StrokeCap.Round
+        colors = ButtonDefaults.buttonColors(
+            containerColor = MaterialTheme.colorScheme.primary,
+            contentColor = MaterialTheme.colorScheme.onPrimary,
+            disabledContainerColor = MaterialTheme.colorScheme.primaryContainer.copy(alpha = 0.9f),
+            disabledContentColor = MaterialTheme.colorScheme.onPrimaryContainer.copy(alpha = 0.9f),
         )
-        else Text(text = text)
+    ) {
+        Box(
+            modifier = modifier.animateContentSize()
+        ) {
+            if (isLoading) CircularProgressIndicator(
+                modifier = Modifier
+                    .size(24.dp)
+                    .aspectRatio(1f),
+                strokeCap = StrokeCap.Round
+            )
+            else Text(
+                text = text,
+                textAlign = TextAlign.Center,
+                modifier = modifier.fillMaxWidth()
+            )
+        }
     }
 }
 
