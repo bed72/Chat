6d3b74d2df023ebbe417de031864ad644c43244d
bed72 <gjramos1000@gmail.com>
2024-12-16 18:00:52 -0300

    :sparkles: Verify token

--- app/src/main/java/com/bed/chat/data/repositories/AuthenticationRepositoryImpl.kt
+++ app/src/main/java/com/bed/chat/data/repositories/AuthenticationRepositoryImpl.kt
@@ -6,6 +6,8 @@ import javax.inject.Inject
 import kotlinx.coroutines.withContext
 import kotlinx.coroutines.CoroutineDispatcher
 
+import com.bed.chat.domain.Constants
+
 import com.bed.chat.external.modules.IoDispatcher
 import com.bed.chat.external.clients.request.toRequest
 
@@ -14,17 +16,20 @@ import com.bed.chat.data.datasources.AuthenticationDatasource
 import com.bed.chat.domain.models.input.SignInInputModel
 import com.bed.chat.domain.models.input.SignUpInputModel
 import com.bed.chat.domain.models.output.ImageOutputModel
+
+import com.bed.chat.domain.repositories.DataStoreRepository
 import com.bed.chat.domain.repositories.AuthenticationRepository
 
 class AuthenticationRepositoryImpl @Inject constructor(
-    private val datasource: AuthenticationDatasource,
-    @IoDispatcher private val ioDispatcher: CoroutineDispatcher
+    private val dataStoreRepository: DataStoreRepository,
+    @IoDispatcher private val ioDispatcher: CoroutineDispatcher,
+    private val authenticationDatasource: AuthenticationDatasource
 ) : AuthenticationRepository {
     @Suppress("ForbiddenComment")
     override suspend fun validateToken(token: String): Result<Unit> =
         withContext(ioDispatcher) {
             runCatching {
-                val response = datasource.validateToken(token)
+                val response = authenticationDatasource.validateToken(token)
 
                 Log.d("[VALIDATE TOKEN]", response.toString())
 
@@ -37,21 +42,23 @@ class AuthenticationRepositoryImpl @Inject constructor(
     override suspend fun signUp(parameter: SignUpInputModel): Result<Unit> =
         withContext(ioDispatcher) {
             runCatching {
-                datasource.signUp(parameter.toRequest()).getOrThrow()
+                authenticationDatasource.signUp(parameter.toRequest()).getOrThrow()
             }
         }
 
     override suspend fun signIn(parameter: SignInInputModel): Result<Unit> =
         withContext(ioDispatcher) {
            runCatching {
-               val response =datasource.signIn(parameter.toRequest())
+               val response = authenticationDatasource.signIn(parameter.toRequest()).getOrDefault(null)
+
+               if (response != null) dataStoreRepository.save(Constants.USER_TOKEN to response.token)
            }
         }
 
     override suspend fun uploadProfilePicture(parameter: String): Result<ImageOutputModel> =
         withContext(ioDispatcher) {
             runCatching {
-                datasource.uploadProfilePicture(parameter).map {
+                authenticationDatasource.uploadProfilePicture(parameter).map {
                     ImageOutputModel(
                         id = it.id,
                         url = it.url,
