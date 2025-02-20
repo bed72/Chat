19947ab80341b81db3ffd8c512d19c34e5bab47d
bed72 <gjramos1000@gmail.com>
2025-02-20 11:07:45 -0300

    :sparkles: finish user screen

--- app/src/main/java/com/bed/chat/presentation/feature/user/UserScreen.kt
+++ app/src/main/java/com/bed/chat/presentation/feature/user/UserScreen.kt
@@ -13,21 +13,26 @@ import androidx.paging.compose.collectAsLazyPagingItems
 
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.fillMaxWidth
 
 import androidx.compose.material3.Text
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.CircularProgressIndicator
 
 import com.bed.chat.R
 
@@ -37,11 +42,11 @@ import com.bed.chat.presentation.shared.theme.ChatTheme
 import com.bed.chat.presentation.shared.components.TopBar
 import com.bed.chat.presentation.shared.components.UserItem
 import com.bed.chat.presentation.shared.components.ChatScaffold
+import com.bed.chat.presentation.shared.components.EmptyContent
 import com.bed.chat.presentation.shared.preview.fake.userOneFake
 import com.bed.chat.presentation.shared.components.PrimaryButton
 import com.bed.chat.presentation.shared.components.FailureContent
 import com.bed.chat.presentation.shared.components.AnimatedContent
-import com.bed.chat.presentation.shared.components.EmptyContent
 import com.bed.chat.presentation.shared.components.skeleton.UserItemSkeleton
 
 @Composable
@@ -140,7 +145,46 @@ private fun Success(
                     HorizontalDivider(modifier = Modifier.padding(top = 16.dp))
                 }
             }
+
+            if (users.loadState.append is LoadState.Loading) item { LoadMore() }
+
+            if (users.loadState.append is LoadState.Error) item { LoadFailure { users.retry() } }
         }
+}
+
+@Composable
+private fun LoadMore() {
+    Box(
+        modifier = Modifier
+            .padding(16.dp)
+            .fillMaxWidth(),
+        contentAlignment = Alignment.Center
+    ) {
+        CircularProgressIndicator()
+    }
+}
+
+@Composable
+private fun LoadFailure(onTryAgainClick: () -> Unit) {
+    Column(
+        modifier = Modifier
+            .padding(16.dp)
+            .fillMaxWidth(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            text = stringResource(R.string.common_generic_error_load_more),
+            modifier = Modifier.padding(8.dp),
+            color = MaterialTheme.colorScheme.onErrorContainer
+        )
+
+        PrimaryButton(
+            text = R.string.common_generic_error_button_retry,
+            onClick = onTryAgainClick,
+            modifier = Modifier.padding(horizontal = 30.dp).height(46.dp)
+        )
+    }
 }
 
 @Composable
